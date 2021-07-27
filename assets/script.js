47694c0851019b26c4533756ebaabda57b177f02
ckitazumi <codykitazumi@gmail.com>
2021-07-26 21:18:13 -0700

    changed website idea and added new api

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,47 @@
-/// fuelApiUrl 
-var maxresults = "&maxresults=10";
-var sortkey = "&sortkey=price";
-var sortorder = "&sortorder=asc";
-/// fuelApiUrl prox = latitude, longitude, radius in meters
-var userProxInput = document.querySelector("select").value;
-var prox = "&prox=" + userProxInput;
-/// fuelApiUrl imagedimensions = w32-h32 (size of brand icon)
-var imagedimensions = "&imagedimensions=";
-/// fuelApiUrl fueltype(s): 1 = Diesel, 2 = Regular, 3 = Mid-grade, 4 = Premium
-var fueltype = "";
-
-var fuelApiUrl = "https://fuel-v2.cc.api.here.com/fuel/stations.json?app_id={}&app_code={}" + maxresults + sortkey + sortorder + prox + fueltype; 
+(async () => {
+    const response = await fetch(
+        'https://parseapi.back4app.com/classes/MichelinGuide_Restaurants?limit=1000&order=-Stars',
+      {
+        headers: {
+          'X-Parse-Application-Id': 'i3w1okhZrfHaMxNFmpor1bzgo7jVF8g8dxbl245e', // This is your app's application id
+          'X-Parse-REST-API-Key': 'l2cZa5ij4wJio7VXQLYZSgolUaSUb1rau3qBc9OC', // This is your app's REST API key
+        }
+      }
+    );
+    const data = await response.json(); // Here you have the data that you need
+    localStorage.setItem("data", JSON.stringify(data));
+  })();
 
 document.querySelector("#search").addEventListener("click", function(event) {
     targetButton = event.target;
 
+    var data = JSON.parse(localStorage.getItem("data"));
+    console.log(data.results.length)
+
     var searchResultDiv = document.createElement("div");
     searchResultDiv.setAttribute("id","searchResultDiv");
 
-    var brandName = document.createElement("h2");
-    brandName.textContent = "Brand Name";
-    searchResultDiv.appendChild(brandName);
+    for (let i = 0; i <= data.results.length; i++) {
+        var restaurantName = document.createElement("h2");
+        restaurantName.textContent = data.results[i].name;
+        searchResultDiv.appendChild(restaurantName);
 
-    var stationAddress = document.createElement("h3");
-    stationAddress.textContent = "Station Address";
-    searchResultDiv.appendChild(stationAddress);
+        var restaurantLocation = document.createElement("h3");
+        restaurantLocation.textContent = data.results[i].city + ", " + data.results[i].region;
+        searchResultDiv.appendChild(restaurantLocation);
 
-    var stationDistance = document.createElement("h3");
-    stationDistance.textContent = "Station Distance";
-    searchResultDiv.appendChild(stationDistance);
+        var restaurantCoordinates = document.createElement("h3");
+        restaurantCoordinates.textContent = data.results[i].Location.latitude + ", " + data.results[i].Location.longitude;
+        searchResultDiv.appendChild(restaurantCoordinates);
 
-    var fuelPrice = document.createElement("h1");
-    fuelPrice.textContent = "Fuel Price";
-    searchResultDiv.appendChild(fuelPrice);
+        var pricePoint = document.createElement("h3");
+        pricePoint.textContent = data.results[i].price;
+        searchResultDiv.appendChild(pricePoint);
 
-    document.querySelector("#resultContainer").appendChild(searchResultDiv);
+        var restaurantStars = document.createElement("h1");
+        restaurantStars.textContent = data.results[i].Stars;
+        searchResultDiv.appendChild(restaurantStars);
+
+        document.querySelector("#resultContainer").appendChild(searchResultDiv);       
+}
 })
