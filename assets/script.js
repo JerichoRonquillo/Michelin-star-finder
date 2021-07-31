bd2b59c5fda33a903892ab2191d49166c8b6f8f7
maxwell walin <maxwellwalin@gmail.com>
2021-07-31 12:07:23 -0700

    created function to edit fetch URL; put fetch inside of a function so it isn't called on page load; editted event listener to input each data result into a div

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,55 @@
-(async () => {
-    const response = await fetch(
-<<<<<<< HEAD
-        'https://parseapi.back4app.com/classes/MichelinGuide_Restaurants?limit=1000', // &order=-Stars (desc), &order=Stars (asc), &order=-Price (desc), &order=Price(asc)
-=======
-        'https://parseapi.back4app.com/classes/MichelinGuide_Restaurants?limit=1000&order=-Stars',
->>>>>>> 47694c0851019b26c4533756ebaabda57b177f02
-      {
-        headers: {
-          'X-Parse-Application-Id': 'i3w1okhZrfHaMxNFmpor1bzgo7jVF8g8dxbl245e', // This is your app's application id
-          'X-Parse-REST-API-Key': 'l2cZa5ij4wJio7VXQLYZSgolUaSUb1rau3qBc9OC', // This is your app's REST API key
+var baseApiUrl
+
+function editURLGivenInput() {
+  baseApiUrl = 'https://parseapi.back4app.com/classes/MichelinGuide_Restaurants?limit=1000';
+
+  var sortBy = document.querySelector("select").value;
+
+  if (sortBy == "starsHighLow") {
+    baseApiUrl += "&order=-Stars";
+  } else if (sortBy == "starsLowHigh") {
+    baseApiUrl += "&order=Stars";
+  } else if (sortBy == "priceHighLow") {
+    baseApiUrl += "&order=-price";
+  } else if (sortBy == "priceLowHigh") {
+    baseApiUrl += "&order=price";
+  }
+}
+
+function fetchRestaurants() {
+  (async () => {
+      const response = await fetch(
+          baseApiUrl, // &order=-Stars (desc), &order=Stars (asc), &order=-Price (desc), &order=Price(asc)
+        {
+          headers: {
+            'X-Parse-Application-Id': 'i3w1okhZrfHaMxNFmpor1bzgo7jVF8g8dxbl245e', // This is your app's application id
+            'X-Parse-REST-API-Key': 'l2cZa5ij4wJio7VXQLYZSgolUaSUb1rau3qBc9OC', // This is your app's REST API key
+          }
         }
-      }
-    );
-<<<<<<< HEAD
-    const data = await response.json(); 
-=======
-    const data = await response.json(); // Here you have the data that you need
->>>>>>> 47694c0851019b26c4533756ebaabda57b177f02
-    localStorage.setItem("data", JSON.stringify(data));
-  })();
+      );
+      const data = await response.json(); // Here you have the data that you need
+      localStorage.setItem("data", JSON.stringify(data));
+    })();
+}
 
 document.querySelector("#search").addEventListener("click", function(event) {
     targetButton = event.target;
 
+    document.querySelector("#resultsContainer").innerHTML = "";
+
+    editURLGivenInput();
+
+    fetchRestaurants();
+
     var data = JSON.parse(localStorage.getItem("data"));
     console.log(data.results.length)
 
-    var searchResultDiv = document.createElement("div");
-    searchResultDiv.setAttribute("id","searchResultDiv");
-
     for (let i = 0; i <= data.results.length; i++) {
+        var index = "Index" + i.toString();
+        var searchResultDiv = document.createElement("div");
+        searchResultDiv.setAttribute("id","searchResultDiv");
+        searchResultDiv.setAttribute("id", index);
+
         var restaurantName = document.createElement("h2");
         restaurantName.textContent = data.results[i].name;
         searchResultDiv.appendChild(restaurantName);
@@ -46,10 +66,10 @@ document.querySelector("#search").addEventListener("click", function(event) {
         pricePoint.textContent = data.results[i].price;
         searchResultDiv.appendChild(pricePoint);
 
-        var restaurantStars = document.createElement("h1");
+        var restaurantStars = document.createElement("h2");
         restaurantStars.textContent = data.results[i].Stars;
         searchResultDiv.appendChild(restaurantStars);
 
-        document.querySelector("#resultContainer").appendChild(searchResultDiv);       
+        document.querySelector("#resultsContainer").appendChild(searchResultDiv);       
 }
 })
